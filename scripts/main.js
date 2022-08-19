66c5544cd905bf903fb16fbcd6a72d75b428fc45
Manion-sebastian <Sjmanion123@gmail.com>
2022-08-19 15:31:35 -0700

    logic improvements, pillar class extended.

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,25 @@
 
 
 // DOM Init Farm
+// Named
 
 const rightDisplay = document.querySelector('#right-aside')
 const leftDisplay = document.querySelector('#left-aside')
 const canvas = document.querySelector('canvas')
+const startMenu = document.querySelector('.startMenu')
+const startButton = document.getElementById('play')
+const optionButton = document.getElementById('options')
+const instructionButton = document.getElementById('inst')
 
-// Object Init Farm
 
+// Anon
+
+document.addEventListener('keydown', movementHandler)
+// canvas.addEventListener('click', (e) => {
+//     console.log(e.offsetX, e.offsetY)
+// })
 
 // Canvas Init
-
-
 
 const ctx = canvas.getContext('2d')
 // canvas.setAttribute('height', getComputedStyle(canvas)['height'])
@@ -40,12 +48,45 @@ class Player {
     }
 }
 
+class Pillar extends Player {
+    constructor(x, y, width, height, color) {
+        super(x, y, width, height, color)
+
+
+    }
+
+    turnOn(color) {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+        ableToKill = true
+
+    }
+}
+
 // Object Init Farm
 
 const dot = new Player(10, 10, 30, 30, 'white')
-const bob = new Player(50, 50, 20, 20, 'red')
+// const bob = new Player(50, 50, 20, 20, 'red') 
 
 // Named Functions 
+
+function startOptions() {
+    
+    startButton.addEventListener('click', () => {
+        startMenu.style.display = 'none'
+        game()
+    })
+
+    optionButton.addEventListener('click', () => {
+        console.log('working')
+        // open options div
+    })
+
+    instructionButton.addEventListener('click', () => {
+        // opens instruction div
+        console.log('working')
+    })
+}
 
 function game() {
     setInterval(gameloop, 60)
@@ -54,35 +95,33 @@ function game() {
 function gameloop() {
     ctx.clearRect(0,0, canvas.width, canvas.height)
     dot.render()
-    bob.render()
+    // bob.render() 
 }
 
-// testing start button functionality. 
-// eventualy try to apply this to the canvas  and have three. 
-// Start, info, and Options. 
-
-function openingMenu() {
-        const btn = document.createElement('button')
-        btn.innerText = "test"
-        btn.classList.add('test')
-        leftDisplay.append(btn)
-        // console.log('working') 
+function movementHandler(e) {
+    // const currentDirection = false dash
+    const speed = 10
+    switch (e.key) {
+        case('w'):
+            dot.y -= speed
+            break
+        case('a'):
+            dot.x -= speed
+            break
+        case('s'):
+            dot.y += speed
+            break
+        case('d'):
+            dot.x += speed
+            break
+    }
     
-    const btnEvent = document.querySelectorAll('button')
-    btnEvent.forEach(btn => {
-        btn.addEventListener('click', () => {
-            game()
-            leftDisplay.removeChild(btn)
-        })  
-    })
 }
 
-canvas.addEventListener('click', (e) => {
-    console.log(e.offsetX, e.offsetY)
-})
+
 
 // Headache Reducer. :)
 
 document.addEventListener('DOMContentLoaded', () => {
-    openingMenu()
+    startOptions()
 })
